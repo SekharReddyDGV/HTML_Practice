4720548b5aa9d84f09e10ca79efb099cbd691da2
Sekhar <sekharreddy825@gmail.com>
2025-03-27 16:41:46 +0530

    HTML_Files

---
+++ MI1.js
@@ -0,0 +1,55 @@
+// var x=10;
+// function fn(){
+//     {
+//     let name ='Raj';
+//     }
+//     console.log(name);
+    
+// }
+// fn();
+
+// var x=5;
+// const y=10;
+//  y=15;
+// console.log(y);
+
+// var car = new Object();
+// car.brand='BMW';
+// car.price=50000000;
+
+// console.log(car.price);
+
+// const target = { a: 1 }; 
+// const source = { b: 2, c: 3 }; 
+// const values= {d:5};
+// Object.assign(source,target,values); 
+// console.log(source); 
+
+// const user = { name: "John" }; 
+// Object.seal(user); 
+// user.name = "Doe"; // Allowed 
+// user.age = 30;  // Not allowed 
+// console.log(user); 
+
+// const target = { a: 1 }; 
+// const source = { b: 2, c: 3 };
+// const value= Object.keys(source);
+// console.log(value);
+
+// let numbers = [10, 20, 30];
+// numbers[2]=40;
+// numbers.push(50,60);
+// console.log(numbers);
+
+// let numbers = [10, 20, 30,40,50,60,70];
+// let Data= numbers.slice(3);
+// console.log(Data);
+
+
+
+for (let i=2; i < 5; i++) { 
+    
+    console.log(i); 
+  } 
+
+
